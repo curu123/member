9bae30e4573f642eed0bd09d1b8504148a0a67d3
curu123 <reflislies@gmail.com>
2026-09-02 17:10:38 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = "rk-member-v11";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then(response =>
+        response || caches.match("./")
+      )
+    )
+  );
+});
